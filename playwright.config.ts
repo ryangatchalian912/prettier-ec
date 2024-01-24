a33c73cd8e5f03ba81dcfe694e59a9377bfeb0a1
Ryan Gatchalian <ryangatchalian912@users.noreply.github.com>
2024-01-24 19:25:23 +0800

    config: Limit failures and fail fast on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  /* Limit the number of failures on CI to save resources */
+  maxFailures: process.env.CI ? 10 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
